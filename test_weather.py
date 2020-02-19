dbc37033bd0562d9e6ea1220963003183955470f
Cagriyoruk <55101879+Cagriyoruk@users.noreply.github.com>
2020-02-19 17:57:34 -0500

    Update test_weather.py

--- test_weather.py
+++ test_weather.py
@@ -5,4 +5,7 @@ def test_weather():
   assert get_airport("Total Rf Helipot") == 'Wrong Input'
   assert get_airport("Total Rf Helport") != None
   assert get_weather("Boston") != None
-
+  with pytest.raises(KeyError):
+    assert(get_weather('New Yoruk'))
+    assert(get_weather('San Zhang'))
+    assert(get_weather('Las Brad'))
