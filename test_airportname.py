f76cd6b36b7c76e164569b87fe26fee76e6895ec
Cagriyoruk <55101879+Cagriyoruk@users.noreply.github.com>
2020-02-19 12:40:55 -0500

    Delete test_airportname.py

--- test_airportname.py
+++
@@ -1,9 +0,0 @@
-from AirportWeatherAPI import *
-import pytest
-
-def test_airport():
-  a_names = ['Wbz Heliport', 'Total Rf Heliport']
-  c_names = ['Boston', 'Bensalem']
-
-  for i in range(len(a_names)):
-    assert get_airport(a_names[i]) == c_names[i] 
